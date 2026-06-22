30189ab2711e77f8550d0f289e997c41db228554
Alessia Foronda <linuxseer@keepmoving.us>
2026-06-21 23:24:41 -0400

    Created variable objects for the DOM elements to be used by JS code.

---
+++ cp_1.js
@@ -0,0 +1,6 @@
+//create objects for each of the input fields
+const form = document.querySelector("form")
+const inputUN = document.getElementById("userName")
+const inputEM = document.getElementById("userEmail")
+const inputC = document.getElementById("comments")
+
