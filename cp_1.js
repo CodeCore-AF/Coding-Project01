d16597e4680ebc32bc75b9504c988da9092206b9
Alessia Foronda <linuxseer@keepmoving.us>
2026-06-21 23:41:43 -0400

    Added Character counter and HTML count display.

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,20 @@
 //create objects for each of the input fields
-const form = document.querySelector("form")
-const inputUN = document.getElementById("userName")
-const inputEM = document.getElementById("userEmail")
-const inputC = document.getElementById("comments")
+const form = document.querySelector("form");
+const inputUN = document.getElementById("userName");
+const inputEM = document.getElementById("userEmail");
+const inputC = document.getElementById("comments");
+const counter = document.querySelector("p");
 
+//Add event listeners to DOM objects
+//Count user character inputs
+let charSum = 0;
+form.addEventListener("input", (event) => {
+    if (event.inputType == "deleteContentBackward" || event.inputType == "deleteContentForward"){
+        charSum -= 1;
+    }else{
+        charSum += 1;
+    }
+    //console.log(event.inputType);
+    console.log(charSum);
+    counter.innerHTML = `Character count: ${charSum}`
+});
